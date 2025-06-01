c322b652417a1902d0a337bf17ce7048c1c3eabd
alanthethinker <144126299+alanthethinker@users.noreply.github.com>
2025-06-02 00:33:51 +0500

    Break test to trigger CI fail

--- tests/test_add.py
+++ tests/test_add.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_add():
     response = client.get("/add?a=1&b=2")
     assert response.status_code == 200
-    assert response.json() == {"result": 3}
+    assert response.json() == {"result": 999}  # ❌ Wrong expected result
